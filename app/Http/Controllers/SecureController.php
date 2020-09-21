f2e809e9f5b7bcf51b30da8faf55cfebe330c056
Armand Iskandarsyah <armand.iskandar96@gmail.com>
2020-09-21 17:21:51 +0700

    Mohon maaf tertinggal pushnya

--- app/Http/Controllers/SecureController.php
+++ app/Http/Controllers/SecureController.php
@@ -2,11 +2,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class SecureController extends Controller
 {
 	public function profile(Request $request)
 	{
-		
+        $response = array(
+            'status' => 'success',
+            'data' => null
+        );
+
+        $explode = explode(' ', $request->header('Authorization'));
+        $response['data'] = User::where('token', $explode[1])->first();
+
+        return response()->json($response);
 	}
 }
